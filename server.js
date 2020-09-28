d7111cfc1b01fb967aa363a6c064305d3409e3d9
odelia <otropper@gmail.com>
2020-09-29 00:32:31 +0300

    added clear history button

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 
-app.get('/:text', (req, res) => {
-    res.send(`<h1>${req.params.text}</h1>`);
+const history = require('./history.json') || [];
+
+app.get('/', (req, res) => {
+    res.send(
+      fs.readFileSync('index.html', {encoding: 'utf-8'})
+    );
 });
 
+app.get('/create-message', (req, res) => {
+    history.push(req.query);
+    fs.writeFileSync('./history.json', JSON.stringify(history,{encoding: 'utf-8'}))
+    res.send(history);
+});
+
+app.get('/display-messages', (req, res) => {
+    res.send(history);
+});
+
+app.get('/clear-history', (req, res) => {
+    history.splice(0,history.length);
+    fs.writeFileSync('./history.json', JSON.stringify(history,{encoding: 'utf-8'}));
+    res.send(history);
+});
 
 app.listen(8080);
