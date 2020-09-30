dd2f90add4e673e2902aeef81fae546a715522e4
odelia <otropper@gmail.com>
2020-09-30 20:10:12 +0300

    added delete+post function

--- server.js
+++ server.js
@@ -4,25 +4,31 @@ const app = express();
 
 const history = require('./history.json') || [];
 
+function saveHistory(history) {
+    fs.writeFileSync('./history.json', JSON.stringify(history,{encoding: 'utf-8'}))
+}
+
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send(
       fs.readFileSync('index.html', {encoding: 'utf-8'})
     );
 });
 
-app.get('/create-message', (req, res) => {
-    history.push(req.query);
-    fs.writeFileSync('./history.json', JSON.stringify(history,{encoding: 'utf-8'}))
+app.post('/create-message', (req, res) => {
+    history.push(req.body);
+    saveHistory(history)
     res.send(history);
 });
 
-app.get('/display-messages', (req, res) => {
+app.get('/messages', (req, res) => {
     res.send(history);
 });
 
-app.get('/clear-history', (req, res) => {
+app.delete('/messages', (req, res) => {
     history.splice(0,history.length);
-    fs.writeFileSync('./history.json', JSON.stringify(history,{encoding: 'utf-8'}));
+    saveHistory(history)
     res.send(history);
 });
 
