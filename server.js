e07738071c1b26dced6ac88fed88068e9de97915
odelia <otropper@gmail.com>
2020-10-01 19:33:44 +0300

    express API post,put,get,delete

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const fs = require('fs');
+const { RSA_NO_PADDING } = require('constants');
 const app = express();
 
 const history = require('./history.json') || [];
+let uuid = (parseFloat(Math.random()).toPrecision(5)) * 1000000;
 
 function saveHistory(history) {
     fs.writeFileSync('./history.json', JSON.stringify(history,{encoding: 'utf-8'}))
@@ -16,17 +18,41 @@ app.get('/', (req, res) => {
     );
 });
 
-app.post('/create-message', (req, res) => {
-    history.push(req.body);
+app.post('/messages', (req, res) => { //create new
+    history.push({
+        id: uuid,
+        ...req.body
+    });
+    uuid+=1;
     saveHistory(history)
     res.send(history);
 });
 
-app.get('/messages', (req, res) => {
+app.put('/messages/:id/:newMessage', (req, res) => { //replace a message
+    for (let i=0 ; i<history.length ; i++)  {
+        if (req.params.id == history[i].id) {
+            history[i].message = req.params.newMessage;
+        }
+    };
+    saveHistory(history);
     res.send(history);
 });
 
-app.delete('/messages', (req, res) => {
+app.delete('/messages/:id', (req, res) => { //delete a message
+    for (let i=0 ; i<history.length ; i++)  {
+        if (req.params.id == history[i].id) {
+            history.splice(i,1);
+        }
+    };
+    saveHistory(history);
+    res.send(history);
+});
+
+app.get('/messages', (req, res) => { //read message
+    res.send(history);
+});
+
+app.delete('/messages', (req, res) => { //delete all
     history.splice(0,history.length);
     saveHistory(history)
     res.send(history);
